5c113039d89120544a31d6b75bfa5abb1ce2484e
Federico Maximovicz <fritzmaximovicz@gmail.com>
2023-11-27 10:56:35 -0300

    added functionality to update user  password

--- controllers/users.js
+++ controllers/users.js
@@ -6,10 +6,10 @@ const User = require('../models/user')
 const Task = require('../models/task')
 
 usersRouter.post('/', async (req, res, next) => {
-  const {email, username, password } = req.body
+  const { email, username, password } = req.body
 
-  if(!password || password.length < 8){
-    return res.status(400).json({error: 'a password is required and it must be longer than 8 characters long'})
+  if (!password || password.length < 8) {
+    return res.status(400).json({ error: 'a password is required and it must be longer than 8 characters long' })
   }
 
   const saltRounds = 10
@@ -17,53 +17,53 @@ usersRouter.post('/', async (req, res, next) => {
 
   const user = new User({
     email,
-    username, 
+    username,
     passwordHash
   })
 
-  try{
+  try {
     const savedUser = await user.save()
     res.status(201).json(savedUser)
-  }catch(error){
+  } catch (error) {
     next(error)
   }
 
 })
 
 usersRouter.get('/', async (req, res, next) => {
-  if(!req.token){
-    return res.status(401).json({error: 'no token provided'})
+  if (!req.token) {
+    return res.status(401).json({ error: 'no token provided' })
   }
 
-  try{
+  try {
     const decodedToken = jwt.verify(req.token, process.env.SECRET)
     const userId = decodedToken.id
     const user = await User.findById(userId).populate('tasks', { name: 1, duration: 1, date: 1 })
     res.json(user)
-  }catch(error){
+  } catch (error) {
     next(error)
   }
 })
 
-usersRouter.patch('/', userExtractor, async (req, res, next) => {
+usersRouter.patch('/updateUsername', userExtractor, async (req, res, next) => {
   const { username } = req.body
 
-  if(!username){
+  if (!username) {
     return res.status(401).json({ error: "no username provided" })
   }
 
-  if(!req.token){
-    return response.status(401).json({error: 'no token provided'})
+  if (!req.token) {
+    return response.status(401).json({ error: 'no token provided' })
   }
 
-  try{
+  try {
     const decodedToken = jwt.verify(req.token, process.env.SECRET)
-    
+
     const userId = decodedToken.id
 
-    const patchedUser = await 
+    const patchedUser = await
       User.findByIdAndUpdate(userId, { $set: { username } }, { new: true })
-    
+
     const tokenUser = {
       username: patchedUser.username,
       id: patchedUser._id
@@ -71,17 +71,53 @@ usersRouter.patch('/', userExtractor, async (req, res, next) => {
 
     const token = jwt.sign(tokenUser, process.env.SECRET)
 
-    res.status(200).send({token, username: patchedUser.username, name: patchedUser.name})
-  }catch(error){
+    res.status(200).send({ token, username: patchedUser.username, name: patchedUser.name })
+  } catch (error) {
     next(error)
   }
 })
 
-usersRouter.get('/sessions', userExtractor, async(req,res,next) => {
-  if(!req.token){
-    return response.status(401).json({error: 'no token provided'})
+usersRouter.patch('/updatePassword', userExtractor, async (req, res, next) => {
+  const { password } = req.body
+
+  if (!password) {
+    return res.status(401).json({ error: 'no new password provided' })
   }
-  try{
+
+  if (!req.token) {
+    return response.status(401).json({ error: 'no token provided' })
+  }
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  try {
+    const decodedToken = jwt.verify(req.token, process.env.SECRET)
+
+    const userId = decodedToken.id
+
+    const patchedUser = await
+      User.findByIdAndUpdate(userId, { $set: { passwordHash } }, { new: true })
+
+    const tokenUser = {
+      username: patchedUser.username,
+      id: patchedUser._id
+    }
+
+    const token = jwt.sign(tokenUser, process.env.SECRET)
+
+    res.status(200).send({ token, username: patchedUser.username, name: patchedUser.name })
+  } catch (error) {
+    next(error)
+  }
+
+})
+
+usersRouter.get('/sessions', userExtractor, async (req, res, next) => {
+  if (!req.token) {
+    return response.status(401).json({ error: 'no token provided' })
+  }
+  try {
     const decodedToken = jwt.verify(req.token, process.env.SECRET)
     if (!decodedToken.id) {
       return response.status(400).json({ error: 'invalid token' })
@@ -93,7 +129,7 @@ usersRouter.get('/sessions', userExtractor, async(req,res,next) => {
     const allSessions = userWithSessions.sessions
 
     res.json(allSessions)
-  }catch(error){
+  } catch (error) {
     console.log(error)
     next(error)
   }
